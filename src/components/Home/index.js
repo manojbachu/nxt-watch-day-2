9c92218dba69653658248b3d922440a0c6f65731
manojbachu <135940834+manojbachu@users.noreply.github.com>
2023-11-01 21:47:38 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,23 @@
 import {Component} from 'react'
-import './styledComponents'
+
+import {HomeContainer, FeaturesContainer} from './styledComponents'
 
 import Header from '../Header'
 
+const apiConstants = {}
+
 class Home extends Component {
+  state = {feature: apiConstants.feature}
+
+  featuresSection = () => {}
+
   render() {
     return (
       <>
         <Header />
+        <HomeContainer>
+          <FeaturesContainer>{this.featuresSection()}</FeaturesContainer>
+        </HomeContainer>
       </>
     )
   }
