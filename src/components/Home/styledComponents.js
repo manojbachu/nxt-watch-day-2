e0609139242880208514921f1cde148a27125e03
manojbachu <135940834+manojbachu@users.noreply.github.com>
2023-11-01 21:48:04 +0530

    Update styledComponents.js

--- src/components/Home/styledComponents.js
+++ src/components/Home/styledComponents.js
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.div``
+export const HomeContainer = styled.div`
+  display: flex;
+`
+
+export const FeaturesContainer = styled.div`
+  width: 100px;
+`
+
+export const VideoSection = styled.div``
