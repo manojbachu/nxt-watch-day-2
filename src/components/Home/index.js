9e285953e7381d1e739fa36ee7c3ba33f9b108cc
manojbachu <135940834+manojbachu@users.noreply.github.com>
2023-11-09 21:58:52 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,7 @@
 import {Component} from 'react'
+import Cookies from 'js-cookie'
+
+import {AiOutlineSearch} from 'react-icons/ai'
 
 import {GrFormClose} from 'react-icons/gr'
 import {
@@ -11,18 +14,66 @@ import {
   PremiumPara,
   GetItNowBtn,
   PremiumAddRemoveBtn,
+  VideosContainer,
+  SearchContainer,
+  SearchInput,
+  SearchButton,
+  VideoListContainer,
 } from './styledComponents'
 
 import Header from '../Header'
 
-const apiConstants = {}
+const apiConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  progress: 'PROGRESS',
+}
 
 class Home extends Component {
-  state = {feature: apiConstants.feature}
+  state = {status: apiConstants.initial, searchInput: '', searchValue: ''}
+
+  componentDidMount() {
+    this.getVideos()
+  }
+
+  onFetchSuccess = data => {}
+
+  getVideos = async () => {
+    const {searchInput} = this.state
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(
+      `https://apis.ccbp.in/videos/all?search=${searchInput}`,
+      options,
+    )
+    const data = await response.json()
+    if (response.ok) {
+      this.onFetchSuccess(data)
+    } else {
+      this.onFetchFailure()
+    }
+    console.log(data)
+  }
 
   featuresSection = () => {}
 
+  onChangeInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  onClickSearch = () => {
+    const {searchInput} = this.state
+    this.setState({searchValue: searchInput})
+  }
+
   render() {
+    const {searchInput} = this.state
     return (
       <>
         <Header />
@@ -44,6 +95,19 @@ class Home extends Component {
                 <GrFormClose />
               </PremiumAddRemoveBtn>
             </PremiumContainer>
+            <VideosContainer>
+              <SearchContainer>
+                <SearchInput
+                  type="search"
+                  onChange={this.onChangeInput}
+                  value={searchInput}
+                />
+                <SearchButton type="button" onClick={this.onClickSearch}>
+                  <AiOutlineSearch />
+                </SearchButton>
+              </SearchContainer>
+              <VideoListContainer>{}</VideoListContainer>
+            </VideosContainer>
           </VideoSection>
         </HomeContainer>
       </>
