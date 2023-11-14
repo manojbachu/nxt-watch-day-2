fcb196817db3714f5efbd6f5b4824a84a689ecf8
manojbachu <135940834+manojbachu@users.noreply.github.com>
2023-11-14 20:27:48 +0530

    Update index.js

--- src/components/VideoItemDetails/index.js
+++ src/components/VideoItemDetails/index.js
@@ -1,16 +1,27 @@
 import {Component} from 'react'
 import Cookies from 'js-cookie'
 
+const apiConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  progress: 'PROGRESS',
+}
+
 class VideoItemDetails extends Component {
-  state = {videoItemDetails: {}}
+  state = {status: apiConstants.initial, videoItemDetails: {}, errMsg: ''}
 
   componentDidMount() {
     this.getVideoDetails()
   }
 
+  onFetchFailure = errMsg => {
+    this.setState({errMsg, status: apiConstants.failure})
+  }
+
   onFetchSuccess = data => {
-    const videoDetails = data.video_details
-    const updatedData = {
+    this.setState({status: apiConstants.progress})
+    const videoItemDetails = {
       id: data.id,
       title: data.title,
       videoUrl: data.video_url,
@@ -24,6 +35,7 @@ class VideoItemDetails extends Component {
       publishedAt: data.published_at,
       description: data.description,
     }
+    this.setState({videoItemDetails, status: apiConstants.success})
   }
 
   getVideoDetails = async () => {
@@ -37,10 +49,13 @@ class VideoItemDetails extends Component {
         Authorization: `Bearer ${jwtToken}`,
       },
     }
-    const response = await fetch(`https://apis.ccbp.in/videos/${id}`, options)
+    const response = await fetch(
+      `https://apis.ccbp.in/videos/${id.slice(1)}`,
+      options,
+    )
     const data = await response.json()
     if (response.ok) {
-      this.onFetchSuccess(data.videos)
+      this.onFetchSuccess(data.video_details)
     } else {
       this.onFetchFailure(data.error_msg)
     }
